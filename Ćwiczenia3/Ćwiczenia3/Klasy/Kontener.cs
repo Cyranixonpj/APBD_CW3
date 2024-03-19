ef11a156b1541645fddb2b8a4d79cdfa86d2b933
Cyranixonpj <s27730@pjwstk.edu.pl>
2024-03-19 20:29:26 +0100

    Kontener2

--- Ćwiczenia3/Ćwiczenia3/Klasy/Kontener.cs
+++ Ćwiczenia3/Ćwiczenia3/Klasy/Kontener.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Ćwiczenia3.Klasy;
 
-public abstract class Kontener(int masaLadunku, int wysokosc,int wagaWlasna) 
+
+
+
+public abstract class Kontener(int masaLadunku, int wysokosc,int wagaWlasna , string typ) 
 {
+    
+    private static int _numer = 0;
+
+    public int Id { get; } = _numer++;
+    public string Typ { get; private set; } = typ;
+    public int Masa { get; private set; } = masaLadunku;
+    public int Wysokosc { get; private set; } = wysokosc;
+    public int WagaWlasna { get; private set; } = wagaWlasna;
+
+
+    
+
+
+
 
 }
